d48f8ae3e02c88c57d97293155c25a0909fb0f1f
Willy Douhard <willy.douhard@gmail.com>
2023-11-24 17:09:07 +0100

    handle decorators and context managers under one function

--- python-client/chainlit_client/client.py
+++ python-client/chainlit_client/client.py
@@ -1,29 +1,37 @@
-import json
 import os
-import uuid
-import inspect
-from functools import wraps
 from typing import Optional
 
 from .api import API
 from .context import active_steps_var, active_thread_id_var
 from .event_processor import EventProcessor
 from .instrumentation.openai import instrument_openai
-from .types import ThreadContextManager, Step, StepContextManager, StepType
+from .types import (
+    thread_decorator,
+    ThreadContextManager,
+    Step,
+    StepContextManager,
+    step_decorator,
+    StepType,
+)
 
 
 class ChainlitClient:
-    def __init__(self, batch_size: int = 1, api_key: Optional[str] = None, endpoint: Optional[str] = None):
+    def __init__(
+        self,
+        batch_size: int = 1,
+        api_key: Optional[str] = None,
+        endpoint: Optional[str] = None,
+    ):
         if not api_key:
-            self.api_key = os.getenv("CHAINLIT_API_KEY", None)
-            if not self.api_key:
+            api_key = os.getenv("CHAINLIT_API_KEY", None)
+            if not api_key:
                 raise Exception("CHAINLIT_API_KEY not provided")
         if not endpoint:
-            self.endpoint = os.getenv(
+            endpoint = os.getenv(
                 "CHAINLIT_ENDPOINT", "https://cloud.chainlit.io/graphql"
             )
 
-        self.api = API(api_key=self.api_key, endpoint=self.endpoint)
+        self.api = API(api_key=api_key, endpoint=endpoint)
         self.event_processor = EventProcessor(
             api=self.api,
             batch_size=batch_size,
@@ -32,74 +40,132 @@ class ChainlitClient:
     def instrument_openai(self):
         instrument_openai(self)
 
-    def step_decorator(
+    def thread(self, original_function=None, *, thread_id: Optional[str] = None):
+        if original_function:
+            return thread_decorator(self, func=original_function, thread_id=thread_id)
+        else:
+            return ThreadContextManager(self, thread_id=thread_id)
+
+    def step(
         self,
-        name: Optional[str] = None,
-        type: Optional[StepType] = None,
+        original_function=None,
+        *,
+        name: str = "",
+        type: StepType = StepType.UNDEFINED,
         thread_id: Optional[str] = None,
     ):
-        def decorator(func):
-            if inspect.iscoroutinefunction(func):
-                @wraps(func)
-                async def async_wrapper(*args, **kwargs):
-                    with StepContextManager(self, name=name or func.__name__, type=type, thread_id=thread_id) as step:
-                        result = await func(*args, **kwargs)
-                        try:
-                            if step.output is None:
-                                step.output = json.dumps(result)
-                        except:
-                            pass
-                        return result
-                return async_wrapper
-            else:
-                @wraps(func)
-                def sync_wrapper(*args, **kwargs):
-                    with StepContextManager(self, name=name or func.__name__, type=type, thread_id=thread_id) as step:
-                        result = func(*args, **kwargs)
-                        try:
-                            if step.output is None:
-                                step.output = json.dumps(result)
-                        except:
-                            pass
-                        return result
-                return sync_wrapper
-        return decorator
-
-
-    def thread_decorator(self, original_function=None, *, thread_id: Optional[str] = None):
-        def decorator(func):
-            if inspect.iscoroutinefunction(func):
-                @wraps(func)
-                async def async_wrapper(*args, **kwargs):
-                    with ThreadContextManager(self, thread_id=thread_id) as step:
-                        result = await func(*args, **kwargs)
-                        return result
-                return async_wrapper
-            else:
-                @wraps(func)
-                def sync_wrapper(*args, **kwargs):
-                    with ThreadContextManager(self, thread_id=thread_id) as step:
-                        return func(*args, **kwargs)
-                return sync_wrapper
-
-        # If the decorator is used without parenthesis, return the decorated function
         if original_function:
-            return decorator(original_function)
+            return step_decorator(
+                self, func=original_function, name=name, type=type, thread_id=thread_id
+            )
+        else:
+            return StepContextManager(self, type=StepType.RUN, thread_id=thread_id)
 
-        # If the decorator is used with parenthesis, return the decorator
-        return decorator
- 
     def run(
         self,
+        original_function=None,
+        *,
+        name: str = "",
         thread_id: Optional[str] = None,
     ):
-        return self.step_decorator(type=StepType.RUN, thread_id=thread_id)
+        if original_function:
+            return step_decorator(
+                self,
+                func=original_function,
+                name=name,
+                type=StepType.RUN,
+                thread_id=thread_id,
+            )
+        else:
+            return StepContextManager(self, type=StepType.RUN, thread_id=thread_id)
 
     def llm(
         self,
+        original_function=None,
+        *,
+        name: str = "",
         thread_id: Optional[str] = None,
     ):
-        return self.step_decorator(type=StepType.LLM, thread_id=thread_id)
+        if original_function:
+            return step_decorator(
+                self,
+                func=original_function,
+                name=name,
+                type=StepType.LLM,
+                thread_id=thread_id,
+            )
+        else:
+            return StepContextManager(self, type=StepType.RUN, thread_id=thread_id)
+
+    def retrieval(
+        self,
+        original_function=None,
+        *,
+        name: str = "",
+        thread_id: Optional[str] = None,
+    ):
+        if original_function:
+            return step_decorator(
+                self,
+                func=original_function,
+                type=StepType.RETRIEVAL,
+                thread_id=thread_id,
+            )
+        else:
+            return StepContextManager(self, type=StepType.RUN, thread_id=thread_id)
+
+    def rerank(
+        self,
+        original_function=None,
+        *,
+        name: str = "",
+        thread_id: Optional[str] = None,
+    ):
+        if original_function:
+            return step_decorator(
+                self,
+                func=original_function,
+                name=name,
+                type=StepType.RERANK,
+                thread_id=thread_id,
+            )
+        else:
+            return StepContextManager(self, type=StepType.RUN, thread_id=thread_id)
+
+    def embedding(
+        self,
+        original_function=None,
+        *,
+        name: str = "",
+        thread_id: Optional[str] = None,
+    ):
+        if original_function:
+            return step_decorator(
+                self,
+                func=original_function,
+                type=StepType.EMBEDDING,
+                thread_id=thread_id,
+            )
+        else:
+            return StepContextManager(self, type=StepType.RUN, thread_id=thread_id)
+
+    def tool(
+        self,
+        original_function=None,
+        *,
+        name: str = "",
+        thread_id: Optional[str] = None,
+    ):
+        if original_function:
+            return step_decorator(
+                self,
+                func=original_function,
+                name=name,
+                type=StepType.TOOL,
+                thread_id=thread_id,
+            )
+        else:
+            return StepContextManager(self, type=StepType.RUN, thread_id=thread_id)
 
     def create_step(
         self,
--- python-client/chainlit_client/context.py
+++ python-client/chainlit_client/context.py
@@ -4,5 +4,5 @@ from typing import TYPE_CHECKING, List, Optional
 if TYPE_CHECKING:
     from .types import Step
 
-active_steps_var = ContextVar[List[Step]]("active_steps", default=[])
+active_steps_var = ContextVar[List["Step"]]("active_steps", default=[])
 active_thread_id_var = ContextVar[Optional[str]]("active_thread", default=None)
