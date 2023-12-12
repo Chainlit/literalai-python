12f5dfe1b76487b8a137109fa376c00f6e65e04a
Pawel Morawian <--get-all>
2023-12-12 14:10:57 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="chainlit_client",
-    version="0.1.0",
+    name="chainlit_python_client",
+    version="0.1.0rc0",
     description="An SDK for observability in Python applications",
     author="",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "asyncio==3.4.3",
+        "packaging==23.2",
+        "httpx>=0.23.0,<0.25.0",
+        "pydantic>=1,<3",
+    ],
 )
