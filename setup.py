54d9c500273f23d35fb1ed5a2ad4b21210cea1e0
Willy Douhard <willy.douhard@gmail.com>
2023-12-18 11:41:31 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="chainlit_client",
-    version="0.1.0rc5",
+    version="0.1.0rc6",
     description="An SDK for observability in Python applications",
     author="",
     package_data={"chainlit_client": ["py.typed"]},
