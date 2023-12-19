391e0abd606ba1d97187d7dfdf4fe6fb43f6dc9b
Thibaut Patel <thibaut.patel@gmail.com>
2023-12-19 12:37:48 +0100

    add client name and version to api-call headers

--- chainlit_client/__init__.py
+++ chainlit_client/__init__.py
@@ -3,10 +3,12 @@ from .message import Message
 from .my_types import *  # noqa
 from .step import Step
 from .thread import Thread
+from .version import __version__
 
 __all__ = [
     "ChainlitClient",
     "Message",
     "Step",
     "Thread",
+    "__version__",
 ]
