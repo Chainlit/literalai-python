13526fd501419b4b1284b018290cce20e3e50122
Clément Sirieix <clementsirieix@clements-mbp.home>
2024-01-05 10:25:25 +0100

    feat/add system message

--- examples/gradio/openai_chat.py
+++ examples/gradio/openai_chat.py
@@ -10,11 +10,21 @@ async_client = AsyncClient(api_key=os.environ.get("OPENAI_KEY"))
 
 async def chat_call(message, history):
     history_openai_format = []
+    history_openai_format.append(
+        {
+            "role": "system",
+            "content": """
+        Your name is Bob, you are an assistant your are a math expert.
+        You only answer math question, if a question is not math related you should respond by pointing out that you are a math expert.
+        """,
+        }
+    )
     for human, assistant in history:
         history_openai_format.append({"role": "user", "content": human})
         history_openai_format.append({"role": "assistant", "content": assistant})
     history_openai_format.append({"role": "user", "content": message})
 
+    print(history_openai_format)
     stream = await async_client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=history_openai_format,
