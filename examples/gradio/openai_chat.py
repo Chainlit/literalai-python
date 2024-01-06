f89ef322787e3b1fb0d007bbe5afa457df51781d
Clément Sirieix <clementsirieix@clements-mbp.home>
2024-01-06 16:14:47 +0100

    feat/refactor first test

--- examples/gradio/openai_chat.py
+++ examples/gradio/openai_chat.py
@@ -11,24 +11,6 @@ sdk = ChainlitClient()
 sdk.instrument_openai()
 
 
-async def call_assistant(messages):
-    with sdk.step(name="Call assistant", type="run"):
-        stream = await async_client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            temperature=1.0,
-            stream=True,
-        )
-        partial_message = ""
-
-        async for chunk in stream:
-            content = chunk.choices[0].delta.content
-
-            if content:
-                partial_message += content
-                yield partial_message
-
-
 async def run(message, history):
     with sdk.thread():
         sdk.message(content=message, type="user_message")
@@ -47,12 +29,30 @@ async def run(message, history):
             history_openai_format.append({"role": "assistant", "content": assistant})
         history_openai_format.append({"role": "user", "content": message})
 
-        response = call_assistant(history_openai_format)
+        response = request_llm(history_openai_format)
         result = ""
         async for chunk in response:
             result = chunk
             yield chunk
         sdk.message(content=result, type="assistant_message", name="Bob")
+
+
+async def request_llm(messages):
+    with sdk.step(name="Ask question", type="run"):
+        stream = await async_client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature=1.0,
+            stream=True,
+        )
+        partial_message = ""
+
+        async for chunk in stream:
+            content = chunk.choices[0].delta.content
+
+            if content:
+                partial_message += content
+                yield partial_message
 
 
 gr.ChatInterface(run).launch()
