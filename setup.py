5b1801d4bff3806476384ecaff91a9f5030ab398
Willy Douhard <willy.douhard@gmail.com>
2024-02-19 11:36:40 +0100

    relax httpx dep

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="literalai",
-    version="0.0.201",  # update version in literalai/version.py
+    version="0.0.202",  # update version in literalai/version.py
     description="An SDK for observability in Python applications",
     author="",
     package_data={"literalai": ["py.typed"]},
     packages=find_packages(),
     install_requires=[
         "packaging>=23.0",
-        "httpx>=0.23.0,<0.25.0",
+        "httpx>=0.23.0,<0.3",
         "pydantic>=1,<3",
     ],
 )
