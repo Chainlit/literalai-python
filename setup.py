ccf7348cc41bf83907015d9e19ee1515137bd5b3
Willy Douhard <willy.douhard@gmail.com>
2024-02-21 18:05:23 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="literalai",
-    version="0.0.205",  # update version in literalai/version.py
+    version="0.0.206",  # update version in literalai/version.py
     description="An SDK for observability in Python applications",
     author="",
     package_data={"literalai": ["py.typed"]},
