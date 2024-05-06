b921177df2f4339383fb6ea21b1f99790c17a243
Brendan Daoud <Granipouss@gmail.com>
2024-05-06 13:33:46 +0200

    feat: rename literal_ to literalai_ (#67)

--- literalai/instrumentation/openai.py
+++ literalai/instrumentation/openai.py
@@ -130,7 +130,7 @@ def instrument_openai(client: "LiteralClient", on_new_generation=None):
                 tools=tools,
                 settings=settings,
                 messages=messages,
-                tags=kwargs.get("literal_tags"),
+                tags=kwargs.get("literalai_tags"),
             )
 
         elif generation_type == GenerationType.COMPLETION:
@@ -157,7 +157,7 @@ def instrument_openai(client: "LiteralClient", on_new_generation=None):
                 model=model,
                 settings=settings,
                 prompt=kwargs.get("prompt"),
-                tags=kwargs.get("literal_tags"),
+                tags=kwargs.get("literalai_tags"),
             )
 
     def update_step_after(
