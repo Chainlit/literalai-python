2d46f8862a41240b7c5baca7c192b2b789fd0377
Willy Douhard <willy.douhard@gmail.com>
2024-05-23 16:23:43 +0200

    add get_my_project_id

--- literalai/api/__init__.py
+++ literalai/api/__init__.py
@@ -21,12 +21,12 @@ from literalai.filter import (
     generations_order_by,
     scores_filters,
     scores_order_by,
+    steps_filters,
+    steps_order_by,
     threads_filters,
     threads_order_by,
     users_filters,
 )
-
-from literalai.filter import steps_filters, steps_order_by
 from literalai.prompt import Prompt, ProviderSettings
 
 from .attachment_helpers import (
@@ -1293,6 +1293,18 @@ class LiteralAPI(BaseLiteralAPI):
         else:
             raise ValueError("Either the `id` or the `name` must be provided.")
 
+    # Misc API
+
+    def get_my_project_id(self):
+        """
+        Retrieves the projectId associated to the API key.
+
+        Returns:
+            The projectId associated to the API key.
+        """
+        response = self.make_rest_call("/my-project", {})
+        return response["projectId"]
+
 
 class AsyncLiteralAPI(BaseLiteralAPI):
     R = TypeVar("R")
@@ -2429,3 +2441,11 @@ class AsyncLiteralAPI(BaseLiteralAPI):
             raise ValueError("Either the `id` or the `name` must be provided.")
 
     get_prompt.__doc__ = LiteralAPI.get_prompt.__doc__
+
+    # Misc API
+
+    async def get_my_project_id(self):
+        response = await self.make_rest_call("/my-project", {})
+        return response["projectId"]
+
+    get_my_project_id.__doc__ = LiteralAPI.get_my_project_id.__doc__
