6d0016fa838f265f098520399a162c03df5f35ec
Dan Constantini <constantinidan@gmail.com>
2024-05-27 14:00:06 +0200

    generate client

---
+++ run-docs.sh
@@ -0,0 +1,4 @@
+# https://github.com/NiklasRosenstein/pydoc-markdown/blob/develop/src/pydoc_markdown/contrib/renderers/markdown.py#L52
+# https://github.com/NiklasRosenstein/pydoc-markdown/blob/develop/src/pydoc_markdown/contrib/processors/filter.py#L31
+
+pydoc-markdown  > ../literal-docs/python-client/client.mdx
