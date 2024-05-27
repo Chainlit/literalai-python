913e65a9208b4b25c0df6bded354ff0f03316ab6
Paul De la Porte <paul.de-la-porte-des-vaux@epita.fr>
2024-05-27 20:08:51 +0200

    added docstrings for client methods and new run-docs script

--- run-docs.sh
+++ run-docs.sh
@@ -1,4 +1,34 @@
-# https://github.com/NiklasRosenstein/pydoc-markdown/blob/develop/src/pydoc_markdown/contrib/renderers/markdown.py#L52
-# https://github.com/NiklasRosenstein/pydoc-markdown/blob/develop/src/pydoc_markdown/contrib/processors/filter.py#L31
+#!/bin/bash
 
-pydoc-markdown  > ../literal-docs/python-client/updated-reference.mdx
+DOCS_DIR=$1
+
+if [ -z "$DOCS_DIR" ]; then
+    echo "Please provide the path to the docs directory"
+    exit 1
+fi
+
+# Define your list
+apiFiles=(
+    "api.__init__"
+)
+
+clientFiles=(
+    "client"
+)
+
+# Loop over the list
+for i in "${apiFiles[@]}"; do
+    echo "Generating docs for $i in api/$i.mdx"
+    # make parent dir if not exists $DOCS_DIR/python-api/$i.mdx
+    mkdir -p $DOCS_DIR/api
+
+    pydoc-markdown -I . -m literalai.$i --no-render-toc > $DOCS_DIR/api/$i.mdx
+done
+
+for i in "${clientFiles[@]}"; do
+    echo "Generating docs for $i in client/$i.mdx"
+    # make parent dir if not exists $DOCS_DIR/python-client/$i.mdx
+    mkdir -p $DOCS_DIR/client
+
+    pydoc-markdown -I . -m literalai.$i --no-render-toc > $DOCS_DIR/client/$i.mdx
+done
