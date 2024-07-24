258ec129761429c1fe6471d107bd63c82a089107
Damien BUTY <dam.buty@gmail.com>
2024-07-24 18:13:20 +0200

    update exampleé

--- examples/llamaindex.py
+++ examples/llamaindex.py
@@ -7,19 +7,13 @@ client.instrument_llamaindex()
 print("Vectorizing documents")
 index = VectorStoreIndex.from_documents([Document.example()])
 
-query_engine = index.as_query_engine()
-# query_engine = index.as_query_engine(streaming=True)
-
 print()
-print("####################")
 print("Sending query")
-print("####################")
 print()
 
 query_engine = index.as_query_engine()
 query_engine.query("Tell me about LLMs?")
 query_engine = index.as_query_engine(streaming=True)
-streaming_response = query_engine.query("Stream : Tell me about LLMs?")
-streaming_response.print_response_stream()
+query_engine.query("Stream : Tell me about LLMs?")
 
 client.flush_and_stop()
