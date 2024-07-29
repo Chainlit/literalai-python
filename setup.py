db0771f2f3b0ec16ed9ce6d66a157f5ac8951ac1
Hugues de Saxcé <hugues@chainlit.io>
2024-07-29 10:16:48 +0200

    chore: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="literalai",
-    version="0.0.609",  # update version in literalai/version.py
+    version="0.0.610",  # update version in literalai/version.py
     description="An SDK for observability in Python applications",
     author="Literal AI",
     package_data={"literalai": ["py.typed"]},
