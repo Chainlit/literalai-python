4124ccee0cd4f799c58a200e2baacd4a759fa98e
Damien BUTY <dam.buty@gmail.com>
2024-07-30 18:14:29 +0200

    fix: imports

--- literalai/__init__.py
+++ literalai/__init__.py
@@ -1,10 +1,10 @@
 from .client import AsyncLiteralClient, LiteralClient
-from literalai.evaluation.dataset import Dataset
-from literalai.observability.message import Message
+from .evaluation.dataset import Dataset
+from .observability.message import Message
 from .evaluation.dataset_item import DatasetItem
 from .my_types import *  # noqa
-from literalai.observability.step import Step
-from literalai.observability.thread import Thread
+from .observability.step import Step
+from .observability.thread import Thread
 from .version import __version__
 
 __all__ = [
